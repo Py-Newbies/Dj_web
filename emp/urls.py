8f7a0c70ed30c2c4398a8b6e2529a72ac324ad7e
Mohan Reddy <thathireddymohanreddy999@gmail.com>
2023-01-12 01:45:20 +0530

    new dashboard synch

--- emp/urls.py
+++ emp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home),
-    path("Login", views.login),
-    path("Signup", views.signup)
+    path("login", views.login, name='login'),
+    path("signup", views.signup)
 ]
--- emp/views.py
+++ emp/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "dashboard.html")
 
 def login(request):
-    return render(request, "login.html")
+    return render(request, "signin.html")
 
 def signup(request):
     return render(request, "signup.html")
+
+
 
