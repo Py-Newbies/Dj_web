2c5e22fec7a718c2b781d89c734afe9a58e6ce2d
saisowmith <saisowmith6471@gmail.com>
2023-01-11 19:19:36 -0500

    signup page is created and also linked to dashboard and signin pages

--- emp/urls.py
+++ emp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home,name='home'),
     path("login", views.login, name='login'),
-    path("signup", views.signup)
+    path("signup", views.signup,name='signup')
 ]
