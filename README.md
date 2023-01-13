57898a86d2f82a66309b0f4b03f4a59113e617bb
Mohan Reddy <thathireddymohanreddy999@gmail.com>
2023-01-13 22:20:30 +0530

    small tinkerings

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Dj_web
+
+<img src="https://pngimg.com/uploads/frog/frog_PNG35774.png" width="200" >
--- emp/views.py
+++ emp/views.py
@@ -4,11 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, "dashboard.html")
 
+
 def login(request):
     return render(request, "signin.html")
 
+
 def signup(request):
     return render(request, "signup.html")
-
-
-
